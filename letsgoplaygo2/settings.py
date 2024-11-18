562f995e462036e57eaeb96bd59b5e18d0492479
Жилин Андрей <152083348+tengenatari@users.noreply.github.com>
2024-11-18 19:04:24 +0500

    return to sqlite

--- letsgoplaygo2/settings.py
+++ letsgoplaygo2/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'letsgoplaygo2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
